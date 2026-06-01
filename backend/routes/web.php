a3e67ac0e81ec4283413ba4f7852581087de119a
Nyckush <nicolas2014velasquez@gmail.com>
2026-06-01 00:03:51 -0300

    Update

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect()->to(Filament::getPanel('admin')->getUrl());
+    }
+
     return view('welcome');
 });
