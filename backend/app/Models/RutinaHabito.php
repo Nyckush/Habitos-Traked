9bac08d6fd71df6fda16f19ec21490184398d178
Nyckush <nicolas2014velasquez@gmail.com>
2026-07-26 14:21:08 -0300

    fix: arregla relacion habito-rutina en backend

--- backend/app/Models/RutinaHabito.php
+++ backend/app/Models/RutinaHabito.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class RutinaHabito extends Model
+class RutinaHabito extends Pivot
 {
     public $timestamps = false;
     public $incrementing = true;
